2c325a44497343ce9e2711aba3f9baf1604edc12
Zakaryan <arshak.zakaryan@effem.com>
2020-06-08 19:36:55 +0200

    one

--- services.js
+++ services.js
@@ -8,8 +8,31 @@ const services = {
   ],
 };
 
-debugger;
+//debugger;
 
 for (key in services) {
-  console.log(key.values);
+  document.write(
+    <div class="col-md-4">
+      <div class="service-box">
+        <div class="service-ico">
+          <a href="./service.html">
+            <span class="ico-circle">
+              <i class="ion-monitor"></i>
+            </span>
+          </a>
+        </div>
+        <div class="service-content">
+          <h2 class="s-title">
+            <a href="./service.html">Web Design</a>
+          </h2>
+          <p class="s-description text-center">
+            Our designers will work with you to understand needs of your's and
+            your customers'. We are happy to create beautiful websites which is
+            a part of your and our success.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+  console.log(services[key]);
 }
