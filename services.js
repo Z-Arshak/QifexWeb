35f4c5d63118addbe2fc25256e25f4d50c74a2f5
Zakaryan <arshak.zakaryan@effem.com>
2020-06-08 15:17:42 +0200

    One

---
+++ services.js
@@ -0,0 +1,15 @@
+const services = {
+  product1: [
+    {
+      name: "web",
+      description: "web desc",
+      img: "web img",
+    },
+  ],
+};
+
+debugger;
+
+for (key in services) {
+  console.log(key.values);
+}
